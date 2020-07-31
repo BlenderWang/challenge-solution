4d55c39455f1b12abfe62e2f4e1f5a65865a6bb3
Shirley Wang <shirleywang527@gmail.com>
2020-07-31 12:06:06 +0200

    initial commit and setup

---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import BrowseSkills from "../img/profile-main-browse-skills.png";
+import InstagramIcon from "../img/profile-details-------------instagram---fontawesome@2x.png";
+
+import "./header.scss";
+
+const Header = () => {
+    const [collapsed, setCollapsed] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        console.log("toggle");
+        setCollapsed(!collapsed);
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="header">
+            <div className="header--left">
+                <i className="fas fa-hashtag"></i>
+                <img
+                    src={BrowseSkills}
+                    alt="browse skills"
+                    className="header--img"
+                />
+            </div>
+            <div className="header--right">
+                <div className="header--icon-burger" onClick={toggle}>
+                    <i className="fas fa-bars"></i>
+                </div>
+
+                <div
+                    className={isOpen ? "header--menu" : `header--menu hidden`}
+                >
+                    <img
+                        src={InstagramIcon}
+                        alt="browse skills"
+                        className="header--icon-instagram"
+                    />
+                    <i className="fab fa-twitter-square"></i>
+                    <h4>
+                        Login
+                        <i className="fas fa-sign-in-alt"></i>
+                    </h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
