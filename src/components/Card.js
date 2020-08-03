90a18722c144bec2e6db731b081a2901d72dbf92
Shirley Wang <shirleywang527@gmail.com>
2020-08-03 17:26:20 +0200

    cleanup

---
+++ src/components/Card.js
@@ -0,0 +1,42 @@
+import React from "react";
+import skillsIcon from "../img/profile-details-------------2bars@2x.png";
+import searchResultsIcon from "../img/search-results--filters-brandenburggate1600 2@2x.png";
+
+const Card = ({ data }) => {
+    return (
+        <section key={data.id}>
+            <div
+                style={{
+                    backgroundImage: `url(${data.src})`,
+                    backgroundPosition: "center 25%",
+                    backgroundSize: "cover",
+                    width: "10rem",
+                    height: "10rem",
+                    borderRadius: "5px",
+                }}
+                key={data.id}
+                className="img-container"
+                id={data.name}
+            ></div>
+            <h3>{data.name}</h3>
+            <p>{data.excerpt}</p>
+            <div className="skills-group">
+                <img src={skillsIcon} alt="bars" />
+                {data.skills}
+            </div>
+
+            <div className="dark">
+                <div className="tags-group">
+                    <i className="fas fa-hashtag"></i>
+                    {data.tags}
+                </div>
+                <div className="location">
+                    <img src={searchResultsIcon} alt="Search Result Icon" />
+                    {data.location}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Card;
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,48 +1,13 @@
 import React from "react";
 import dummyData from "../dummyData.js";
-import skillsIcon from "../img/profile-details-------------2bars@2x.png";
-import searchResultsIcon from "../img/search-results--filters-brandenburggate1600 2@2x.png";
+import Card from "./Card";
 import "./details.scss";
 
 const Details = () => {
     return (
         <div className="details-container">
             {dummyData.details.map((data) => (
-                <section key={data.id}>
-                    <div
-                        style={{
-                            backgroundImage: `url(${data.src})`,
-                            backgroundPosition: "center 25%",
-                            backgroundSize: "cover",
-                            width: "10rem",
-                            height: "10rem",
-                            borderRadius: "5px",
-                        }}
-                        key={data.id}
-                        className="img-container"
-                        id={data.name}
-                    ></div>
-                    <h3>{data.name}</h3>
-                    <p>{data.excerpt}</p>
-                    <div className="skills-group">
-                        <img src={skillsIcon} alt="bars" />
-                        {data.skills}
-                    </div>
-
-                    <div className="dark">
-                        <div className="tags-group">
-                            <i className="fas fa-hashtag"></i>
-                            {data.tags}
-                        </div>
-                        <div className="location">
-                            <img
-                                src={searchResultsIcon}
-                                alt="Search Result Icon"
-                            />
-                            {data.location}
-                        </div>
-                    </div>
-                </section>
+                <Card data={data} />
             ))}
         </div>
     );
