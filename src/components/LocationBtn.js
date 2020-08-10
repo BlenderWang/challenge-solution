8af0e9a5eea34ae400893b44f6f79194c837f04d
Shirley Wang <shirleywang527@gmail.com>
2020-08-10 09:55:10 +0200

    radio btn small hack

--- src/components/LocationBtn.js
+++ src/components/LocationBtn.js
@@ -2,11 +2,12 @@ import React from "react";
 
 const LocationBtn = ({ text, onClick, color }) => {
     return (
-        <div className={color ? "btn-group red" : "btn-group"}>
+        <div
+            className={color ? "btn-group red" : "btn-group"}
+            onClick={onClick}
+        >
             <i className="fas fa-circle"></i>
-            <button className="btn" onClick={onClick}>
-                {text}
-            </button>
+            <button className="btn">{text}</button>
         </div>
     );
 };
